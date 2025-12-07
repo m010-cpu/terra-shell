7d3fad0b35fe2a57260b504b0099146a50679a1d
m010-cpu <81363508+m010-cpu@users.noreply.github.com>
2025-12-07 16:59:35 +0100

    fix style + add gitignore

--- app.tsx
+++ app.tsx
@@ -2,10 +2,12 @@ import app from "ags/gtk4/app";
 import Bar from "./widget/Bar";
 import { Gtk } from "ags/gtk4";
 
+import style from "./style.css";
+
 Gtk.Settings.get_default().gtk_icon_theme_name = "Adwaita";
 
 app.start({
-  css: "./style.css",
+  css: style,
   main() {
     Bar(0);
   },
